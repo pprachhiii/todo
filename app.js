9006ec4259bb3a30707afc6e48ba161ab26c5102
Prachi Yadav <prachiiyadav2409@gmail.com>
2025-03-07 22:06:11 +0530

    added due date feature

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const taskInput = document.querySelector(".taskInput");
+const dueDateInput = document.querySelector(".dueDate");
 const addtaskBtn = document.querySelector(".addTaskBtn");
 const deletetaskBtn = document.querySelector(".deleteTaskBtn");
 const taskList = document.querySelector(".task ul");
 
 addtaskBtn.addEventListener("click",()=>{
-    if(taskInput.value.trim() !==""){
+    if(taskInput.value.trim() !=="" && dueDateInput.value !== ""){
         const li = document.createElement("li");
         li.innerText = taskInput.value;
+
+        const dueDate = document.createElement("span");
+        dueDate.innerText = `(Due: ${dueDateInput.value})`;
+
 
         const removeBtn = document.createElement("button");
         removeBtn.innerText = "X";
@@ -21,13 +26,15 @@ addtaskBtn.addEventListener("click",()=>{
             if(doneBtn.checked){
                 li.style.textDecoration = "line-through";
                 taskList.appendChild(li);
-                doneBtn.style.display = "none"
+                doneBtn.style.display = "none";
+                dueDate.style.display = "none";
             }else{
                 li.style.textDecoration = "none";  
             }
             
         });
-        
+
+        li.appendChild(dueDate);
         li.appendChild(doneBtn);
         li.appendChild(removeBtn);
         taskList.appendChild(li);
